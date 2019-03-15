2ca755d27667ee13336a58ffc71aabb4491f85ec
Robert Komaromi <robertkomaromi@gmail.com>
2019-03-15 17:07:42 -0400

    Refactor date picker component

--- src/components/FormDatePicker.vue
+++ src/components/FormDatePicker.vue
@@ -2,23 +2,17 @@
     <div class="form-group">
         <label v-uni-for="name">{{label}}</label>
         <datetime
-                type="datetime"
-                v-model="content"
-                :value="content"
-                @input="updateValue"
-                :input-class="dtClass"
-                :value-zone="dtZoneServer"
-                :zone="dtZoneClient"
+                v-on="$listeners"
+                v-bind="$attrs"
+                :type="type"
+                :input-class="inputClass"
+                :value-zone="valueZone"
+                :zone="zone"
                 :title="placeholder"
-                :format="dtFormat"
-                :phrases="dtPhrases"
-                :hour-step="dtHourStep"
-                :minute-step="dtMinuteStep"
-                :min-datetime="dtMin"
-                :max-datetime="dtMax"
-                :week-start="7"
-                :use12-hour="dtUse12Hour"
-                auto
+                :week-start="weekStart"
+                :format="format"
+                :phrases="parsedPhrases"
+                :auto="auto"
         ></datetime>
 
         <div v-if="(validator && validator.errorCount) || error" class="invalid-feedback">
@@ -32,157 +26,86 @@
 
 <script>
   /* global ProcessMaker*/
-  import {createUniqIdsMixin} from "vue-uniq-ids";
-  import ValidationMixin from "./mixins/validation";
+  import {createUniqIdsMixin} from 'vue-uniq-ids';
+  import ValidationMixin from './mixins/validation';
   import {Datetime} from 'vue-datetime';
-
-  // You need a specific loader for CSS files
   import 'vue-datetime/dist/vue-datetime.css'
 
-  // Create the mixin
   const uniqIdsMixin = createUniqIdsMixin();
+
   export default {
+    inheritAttrs: false,
     mixins: [uniqIdsMixin, ValidationMixin],
     components: {
       datetime: Datetime
     },
-    props: [
-      'label',
-      'error',
-      'helper',
-      'type',
-      'name',
-      'placeholder',
-      'value',
-      'minDatetime',
-      'maxDatetime',
-      'format',
-      'phrases',
-      'hourStep',
-      'use12Hour',
-      'minuteStep',
-      'zoneServer',
-      'zoneClient',
-      'controlClass',
-    ],
-    computed: {
-      dtClass() {
-        let controlClass = 'form-control';
-        if (this.controlClass) {
-          controlClass = this.controlClass;
-        }
-        return controlClass;
-      },
-      dtMin() {
-        if (this.minDatetime) {
-          return this.minDatetime;
-        }
-        return "1";
-      },
-      dtMax() {
-        if (this.maxDatetime) {
-          return this.maxDatetime;
-        }
-        return "1";
-      },
-      dtFormat() {
-        //If the global variable is set by default.
-        if (typeof ProcessMaker !== "undefined" && ProcessMaker.user && ProcessMaker.user.calendar_format) {
-          return ProcessMaker.user.calendar_format;
-        }
-        if (this.format && typeof this.format === 'string') {
-          return this.format;
-        }
-        let format = {};
-        format.year = 'numeric';
-        format.month = 'numeric';
-        format.day = 'numeric';
-        format.hour = 'numeric';
-        format.minute = '2-digit';
-        if (this.format && typeof this.format === 'object') {
-          format.year = this.format.year ? this.format.year : format.year;
-          format.month = this.format.month ? this.format.month : format.month;
-          format.day = this.format.day ? this.format.day : format.day;
-          format.hour = this.format.hour ? this.format.hour : format.hour;
-          format.minute = this.format.minute ? this.format.minute : format.minute;
-
-          if (this.format.timeZoneName) {
-            format.timeZoneName = this.format.timeZoneName;
+    props: {
+      name: String,
+      placeholder: String,
+      label: String,
+      error: String,
+      helper: String,
+      type: { type: String, default: 'datetime' },
+      inputClass: { type: [String, Array, Object], default: 'form-control' },
+      valueZone: {
+        type: String,
+        default() {
+          if (typeof ProcessMaker !== 'undefined' && ProcessMaker.user && ProcessMaker.user.app_timezone) {
+            return ProcessMaker.user.app_timezone;
           }
+
+          return 'UTC';
         }
-        return format;
       },
-      dtPhrases() {
-        let phrases = {};
-        phrases.ok = 'Continue';
-        phrases.cancel = 'Exit';
-        if (this.phrases && typeof this.phrases === 'string') {
+      zone: {
+        type: String,
+        default() {
+          if (typeof ProcessMaker !== 'undefined' && ProcessMaker.user && ProcessMaker.user.timezone) {
+            return ProcessMaker.user.timezone;
+          }
+
+          return 'local';
+        }
+      },
+      weekStart: { type: Number, default: 7 },
+      format: {
+        type: [String, Object],
+        default() {
+          if (typeof ProcessMaker !== 'undefined' && ProcessMaker.user && ProcessMaker.user.calendar_format) {
+            return ProcessMaker.user.calendar_format;
+          }
+
+          return {
+            year: 'numeric',
+            month: 'numeric',
+            day: 'numeric',
+            hour: 'numeric',
+            minute: '2-digit',
+          };
+        }
+      },
+      phrases: {
+        type: [String, Object],
+        default() {
+          return { ok: 'Continue', cancel: 'Exit' };
+        }
+      },
+      auto: { type: Boolean, default: true },
+    },
+    computed: {
+      parsedPhrases() {
+        let phrases;
+
+        if (typeof this.phrases === 'string') {
           try {
             phrases = JSON.parse(this.phrases)
           } catch (e) {
-            //phrases with values by default
+            // Ignore string, use default prop
           }
         }
-        if (this.phrases && typeof this.phrases === 'object') {
-          phrases.ok = this.phrases.ok ? this.phrases.ok : phrases.ok;
-          phrases.cancel = this.phrases.cancel ? this.phrases.cancel : phrases.cancel;
-        }
-        return phrases;
-      },
-      dtHourStep() {
-        if (this.hourStep) {
-          return parseInt(this.hourStep);
-        }
-        return 1;
-      },
-      dtMinuteStep() {
-        if (this.minuteStep) {
-          return parseInt(this.minuteStep);
-        }
-        return 1;
-      },
-      dtZoneServer() {
-        //If the global variable is set by default.
-        if (typeof ProcessMaker !== "undefined" && ProcessMaker.user && ProcessMaker.user.app_timezone) {
-          return ProcessMaker.user.app_timezone;
-        }
-        if (this.zoneServer) {
-          return this.zoneServer;
-        }
-        return 'UTC';
-      },
-      dtZoneClient() {
-        //If the global variable is set by default.
-        if (typeof ProcessMaker !== "undefined" && ProcessMaker.user && ProcessMaker.user.timezone) {
-          return ProcessMaker.user.timezone;
-        }
-        if (this.zoneClient) {
-          return this.zoneClient;
-        }
-        return 'local'
-      },
-      dtUse12Hour() {
-        return this.use12Hour ? Boolean(this.use12Hour) : false;
-      }
-    },
-    data() {
-      return {
-        content: null,
-        validator: null
-      };
-    },
-    watch: {
-      value() {
-        this.content = this.value;
-      }
-    },
-    methods: {
-      updateValue(newDate) {
-        this.$emit("input", newDate);
+
+        return { ok: 'Continue', cancel: 'Exit' };
       }
     }
   };
 </script>
-
-<style lang="scss" scoped>
-</style>
