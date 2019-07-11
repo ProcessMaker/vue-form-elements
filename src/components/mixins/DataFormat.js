c5add7f9c4f339d1699cf75e6ffbccf6d81a0a22
David Callizaya <davidcallizaya@gmail.com>
2019-07-11 10:38:55 -0400

    Fix code

--- src/components/mixins/DataFormat.js
+++ src/components/mixins/DataFormat.js
@@ -1,4 +1,4 @@
-let Validator = require('validatorjs');
+import Validator from 'validatorjs';
 
 // To include another language in the Validator with variable processmaker
 if (window.ProcessMaker && window.ProcessMaker.user && window.ProcessMaker.user.lang) {
@@ -7,6 +7,7 @@ if (window.ProcessMaker && window.ProcessMaker.user && window.ProcessMaker.user.
 
 export default {
   props: {
+    transientData: Object,
     dataFormat: {
       type: String,
       default() {
@@ -22,8 +23,8 @@ export default {
   watch: {
     // Triggered whenever the v-model is updated
     value() {
-      if (this.$parent.transientData) {
-        this.$parent.transientData[this.name] = this.formatValue(this.value);
+      if (this.transientData) {
+        this.transientData[this.name] = this.formatValue(this.value);
       }
     },
   },
@@ -36,9 +37,10 @@ export default {
       if (!value && this.dataFormat !== 'boolean') {
         return '';
       }
-
-      //Rules
-      let rules = {
+      return this.validateRuleFormat(value) ? this.formatValueIfValid(value) : value;
+    },
+    validateRuleFormat(value) {
+      const rules = {
         'int': 'integer',
         'boolean': 'boolean',
         'string': 'string',
@@ -47,46 +49,37 @@ export default {
         'float': 'regex:/^[+-]?\\d+(\\.\\d+)?$/',
         'currency': 'regex:/^\\d{1,3}(,\\d{3})*(\\.\\d\\d)?(\\D{0,3})$/',
       };
-      //Validate rule format
       this.validatorFormat = new Validator( {[this.name]: value}, {[this.name]: rules[this.dataFormat]}, null);
-
-      let newValue = value;
-      if (this.validatorFormat.passes()) {
-        this.validatorFormat = null;
-        switch (this.dataFormat) {
-          case 'string':
-            newValue = newValue.toString();
-            break;
-          case 'boolean':
-            newValue = Boolean(newValue);
-            break;
-          case 'currency':
-            newValue = newValue.toString();
-            break;
-          case 'date':
-            //to_do format
-            newValue = newValue.toString();
-            //newValue = new Date(newValue).toISOString().substr(0,10);
-            break;
-          case 'datetime':
-            //to_do format
-            newValue = newValue.toString();
-            /*let date = new Date(newValue).toISOString();
-            newValue = date.substr(0,10);
-            newValue= newValue + ' ' + date.substr(11, 8);*/
-            break;
-          case 'float':
-            newValue = Number(newValue);
-            break;
-          case 'int':
-            newValue = parseInt(newValue);
-            break;
-          default:
-            newValue = newValue.toString();
-            break;
-        }
+      return this.validatorFormat.passes();
+    },
+    formatValueIfValid(newValue) {
+      this.validatorFormat = null;
+      switch (this.dataFormat) {
+        case 'string':
+          newValue = newValue.toString();
+          break;
+        case 'boolean':
+          newValue = Boolean(newValue);
+          break;
+        case 'currency':
+          newValue = newValue.toString();
+          break;
+        case 'date':
+          newValue = newValue.toString();
+          break;
+        case 'datetime':
+          newValue = newValue.toString();
+          break;
+        case 'float':
+          newValue = Number(newValue);
+          break;
+        case 'int':
+          newValue = parseInt(newValue);
+          break;
+        default:
+          newValue = newValue.toString();
+          break;
       }
-
       return newValue;
     },
   },
