8e60e4b3cc2fb882e8009c472db1ccaaaa2337e6
David Callizaya <davidcallizaya@gmail.com>
2019-08-28 14:51:44 -0400

    If value is cleaned it is set to Default

--- src/components/mixins/hasDefaultOptionKey.js
+++ src/components/mixins/hasDefaultOptionKey.js
@@ -6,5 +6,11 @@ export default {
         !this.value ? this.$emit('input', value) : null;
       }
     },
+    value: {
+      immediate: true,
+      handler() {
+        !this.value && this.options.defaultOptionKey ? this.$emit('input', this.options.defaultOptionKey) : null;
+      }
+    },
   },
 }
