c5563f13aad2716796a35fd647d77947ddd9f24c
Jeremy Cook <jeremycook0@icloud.com>
2019-10-18 15:05:05 -0400

    Added a simple test for demo purposes.

--- tests/unit/formInput.spec.js
+++ tests/unit/formInput.spec.js
@@ -1,9 +1,19 @@
 import { shallowMount } from '@vue/test-utils'
-import FormInput from "../../src/components/FormInput.vue";
+import FormInput from '../../src/components/FormInput.vue';
 
 describe('FormInput', () => {
   it('can create the component', () => {
     const wrapper = shallowMount(FormInput);
     expect(wrapper.html()).toContain('input');
-  })
+  });
+
+  it('should display helper text', function () {
+    const helperText = "This is some text";
+    const wrapper = shallowMount(FormInput, {
+      propsData: {
+        helper: helperText
+      }
+    });
+    expect(wrapper.html()).toContain(helperText);
+  });
 });
