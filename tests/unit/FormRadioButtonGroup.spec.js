22d54fa937c61951e87d9d8d5532c2aa4ff97d31
sanjacornelius <sanja.cornelius@processmaker.com>
2019-11-08 12:51:33 -0800

    Refactor: FormRadioButtonGroup Tests

--- tests/unit/FormRadioButtonGroup.spec.js
+++ tests/unit/FormRadioButtonGroup.spec.js
@@ -2,161 +2,157 @@ import { shallowMount } from '@vue/test-utils'
 import FormRadioButton from '../../src/components/FormRadioButtonGroup.vue';
 
 describe('FormRadioButton', () => {
-    let wrapper;
-    const optionsArray =  [
-        {
-            'value': 'foo',
-            'content': 'Foo'
-        },
-        {
-            'value': 'bar',
-            'content': 'Bar'
-        }
-    ];
-    const json = JSON.stringify(optionsArray);
+  const factory = (propsData) => {
+    return shallowMount(FormRadioButton, { propsData });
+  }
+  const options =  [
+    {
+        'value': 'foo',
+        'content': 'Foo'
+    },
+    {
+        'value': 'bar',
+        'content': 'Bar'
+    }
+  ];
+  const json = JSON.stringify(options);
+  
+  it('renders the component', () => {
+    const wrapper = factory({ options });
 
-    beforeEach(() => {
-        wrapper = shallowMount(FormRadioButton, {
-            propsData: {
-                options: optionsArray
-            }
-        });
+    expect(wrapper.html()).toContain('input');
+    expect(wrapper.find('input').attributes().type).toBe('radio');
+    expect(wrapper.findAll('input').length).toBe(options.length); 
+  });
+
+  it('renders the component as a data source', () => {
+    const wrapper = factory({
+      options: {
+        dataSource: 'provideData',
+        jsonData: json
+      }
     });
 
-    it('renders the component', () => {
-        expect(wrapper.find('input').attributes().type).toBe('radio');
-        debugger;
+    expect(wrapper.html()).toContain('input');
+    expect(wrapper.find('input').attributes().type).toBe('radio');
+    expect(wrapper.findAll('input').length).toBe(options.length);
+  });
+
+  it('should have an empty value on mount', () => {
+    const wrapper = factory({ options });
+    expect(wrapper.vm.value).toBe(undefined);
+  });
+
+  it('should emit a value on mount', () => {
+    const wrapper = factory({ options });
+    expect(wrapper.emitted().input).toBeTruthy();    
+  });
+
+  it('should emit the value when input changes', () => {
+    const wrapper = factory({ options });
+    const radioOptions = wrapper.findAll('input');
+    const value = radioOptions.at(1);
+    
+    value.setChecked();
+    expect(wrapper.emitted().input[1]).toEqual([value.element.value]);
+  });
+
+  it('sets the value on input', () => {
+    const wrapper = factory({ options });
+    const value = 'foo';
+
+    wrapper.setProps({ value });
+    const radioOptions = wrapper.findAll('input');
+    expect(radioOptions.at(0).element.checked).toBe(true);
+  });
+
+  it('should update the value when a initial value is set and the input changes', () => {
+    const value = 'foo';
+    const newVal = 'bar';
+    const wrapper = factory({ 
+      options: options,
+      value: value
+     });
+    const radioOptions = wrapper.findAll('input');
+    expect(radioOptions.at(0).element.checked).toBe(true);
+    
+    wrapper.setProps({
+      value: newVal
+    });
+    expect(radioOptions.at(1).element.checked).toBe(true);
+    expect(radioOptions.at(0).element.checked).toBe(false);
+  });
+
+  it('renders all configured properties', () => {
+    const label = "Form Radio Label";
+    const helper = "This is some text";
+    const name = "FormRadioButton";
+    const defaultOptionKey = 'foo';
+    const wrapper = factory({
+      label: label,
+      helper: helper,
+      name: name,
+      toggle: true,
+      defaultOptionKey: defaultOptionKey,
+      disabled: true,
+      options: options
+    });
+    
+    expect(wrapper.html()).toContain(label);
+    expect(wrapper.html()).toContain(helper);
+    expect(wrapper.find('input').attributes().name).toBe(name);
+    expect(wrapper.find('input').attributes().disabled).toBe('disabled');
+    expect(wrapper.find('input').classes('custom-control-input')).toBe(true);
+
+    const radioInputs = wrapper.findAll('input');
+    expect(radioInputs.at(0).element.checked).toBe(true);
+  });
+
+  it('renders all configured properties for a data source', () => {
+    const dataSource = 'provideData';
+    const defaultOptionKey = 'bar';
+
+    wrapper.setProps({
+        dataSource: dataSource,
+        defaultOptionKey: defaultOptionKey,
+        jsonData: json    
+    });
+    
+    const radioInputs = wrapper.findAll('input');
+    expect(radioInputs.at(1).element.checked).toBe(true);
+  
+    expect(wrapper.props().options.dataSource).toBe(dataSource);
+    expect(wrapper.props().options.jsonData).toBe(json);
+  });
+
+  it('displays validation error messages when the field is invalid', () => {
+    const requiredText = 'The FormRadioButton field is required.';
+    const errorText = 'This field has an error';
+    const wrapper = factory({
+      name: 'FormRadioButton',
+      error: errorText,
+      validation: 'required',
+      options: options
     });
 
-    it('renders the component with the "provideData" data source', () => {
-        wrapper.setProps({
-            dataSource: 'provideData',
-            jsonData: json    
-        });
-        expect(wrapper.find('input').attributes().type).toBe('radio');
+    expect(wrapper.find('input').classes('is-invalid')).toBe(true);
+    expect(wrapper.find('.invalid-feedback').isVisible()).toBe(true);
+    expect(wrapper.find('.invalid-feedback').text()).toContain(requiredText);
+    expect(wrapper.find('.invalid-feedback').text()).toContain(errorText);
+  });
+
+  it('removes the validation error messages when the field is valid.', () => {
+    const errorText = 'This field has an error';
+    const value = 'bar';
+    const wrapper = factory({
+      name: 'FormRadioButton',
+      error: errorText,
+      validation: 'required',
+      options: options
     });
-
-    // TODO: Write a test for the dataObject data source.
-
-    it('renders all configured properties', () => {
-        const label = "Form Radio Label";
-        const helper = "This is some text";
-        const name = "namehere";
-        const errorText = "This field has an error";
-        const defaultOptionKey = 'foo';
-        const toggle = true;
-
-        wrapper.setProps({
-            label: label,
-            helper: helper,
-            name: name,
-            error: errorText,
-            toggle: toggle,
-            defaultOptionKey: defaultOptionKey,
-            disabled: true,
-        });
-        
-        expect(wrapper.html()).toContain(label);
-        expect(wrapper.find('.form-text').isVisible()).toBe(true);
-        expect(wrapper.find('.form-text').text()).toBe(helper);
-        expect(wrapper.find('input').attributes().name).toBe(name);
-
-        expect(wrapper.find('.invalid-feedback').exists()).toBe(true);
-        expect(wrapper.find('.invalid-feedback').isVisible()).toBe(true);
-        expect(wrapper.find('.invalid-feedback').text()).toBe(errorText);
-        expect(wrapper.find('.custom-radio').isVisible()).toBe(true);
-        expect(wrapper.vm.value).toBe(defaultOptionKey);
-        expect(wrapper.find('input').attributes().disabled).toBe('disabled');
-    });
-
-    it('renders all configured properties for the "provideData" data source', () => {
-        const dataSource = 'provideData';
-        const defaultOptionKey = 'bar';
-
-        wrapper.setProps({
-            dataSource: dataSource,
-            defaultOptionKey: defaultOptionKey,
-            jsonData: json    
-        });
-        
-        const radioInputs = wrapper.findAll('input');
-        expect(radioInputs.at(1).element.checked).toBe(true);
-        expect(wrapper.vm.value).toBe(defaultOptionKey);
-
-        expect(wrapper.props().options.dataSource).toBe(dataSource);
-        expect(wrapper.props().options.jsonData).toBe(json);
-    });
-
-    // TODO: Write a test for the dataObject data source
-
-    it('updates and emits the radio button v-model', () => {
-        // Should emit on mount
-        expect(wrapper.emitted().input).toBeTruthy();
-
-        wrapper.setProps({
-            value: 'bar'
-        });
-        
-        expect(wrapper.vm.value).toBe('bar');
-        const radioInputs = wrapper.findAll('input');
-        expect(radioInputs.at(1).element.checked).toBe(true);
-
-        radioInputs.at(0).setChecked();
-        expect(radioInputs.at(1).element.checked).toBe(false);
-        expect(radioInputs.at(0).element.checked).toBe(true);
-        expect(wrapper.vm.value).toBe('foo');
-        
-        expect(wrapper.emitted().input.length).toBe(2);
-        expect(wrapper.emitted().input[1]).toEqual(['foo']);
-    });
-
-    it('updates and emits the radio button v-model for the "provideData" data source', () => {
-        // Should emit on mount
-        expect(wrapper.emitted().input).toBeTruthy();
-        
-        wrapper.setProps({
-            value: 'bar',
-            options: {
-                dataSource: 'provideData',
-                jsonData: json
-            }
-        });
-        expect(wrapper.vm.value).toBe('bar');
-        const radioInputs = wrapper.findAll('input');
-        expect(radioInputs.at(1).element.checked).toBe(true);
-
-        radioInputs.at(0).setChecked();
-        expect(radioInputs.at(1).element.checked).toBe(false);
-        expect(radioInputs.at(0).element.checked).toBe(true);
-        expect(wrapper.vm.value).toBe('foo');
-        
-        expect(wrapper.emitted().input.length).toBe(2);
-        expect(wrapper.emitted().input[1]).toEqual(['foo']);
-    });
-
-    it('runs validation', () => {
-        const requiredText = 'The FormRadioButton is an required field.';
-        const invalidSelectText = 'The selected FormRadioButton is invalid.';
-
-        wrapper.setProps({
-            name: "FormRadioButton",
-            validation: "required|in:bar"
-        });
-        expect(wrapper.find('.is-invalid').exists()).toBe(true);
-        expect(wrapper.find('.invalid-feedback').exists()).toBe(true);
-        expect(wrapper.find('.invalid-feedback').isVisible()).toBe(true);
-        expect(wrapper.find('.invalid-feedback').isVisible()).toBe(requiredText);
-
-        const radioInputs = wrapper.findAll('input');
-        radioInputs.at(0).setChecked();
-        
-        expect(wrapper.find('.is-invalid').exists()).toBe(true);
-        expect(wrapper.find('.invalid-feedback').exists()).toBe(true);
-        expect(wrapper.find('.invalid-feedback').isVisible()).toBe(true);
-        expect(wrapper.find('.invalid-feedback').text()).toBe(invalidSelectText);
-
-        radioInputs.at(1).setChecked();
-        expect(wrapper.find('.is-invalid').exists()).toBe(false);
-        expect(wrapper.find('.invalid-feedback').exists()).toBe(false);
-    });
+    
+    wrapper.setProps({ value });
+    expect(wrapper.find('.invalid-feedback').exists()).toBe(false);
+    expect(wrapper.find('input').classes('is-invalid')).toBe(false);
+  });
 });
