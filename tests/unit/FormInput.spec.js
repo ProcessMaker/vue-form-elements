ca81cbde9b618ade341c10faff1343b60e5698bd
sanjacornelius <sanja.cornelius@processmaker.com>
2019-11-08 09:21:30 -0800

    Refactor: FormInput Tests

--- tests/unit/FormInput.spec.js
+++ tests/unit/FormInput.spec.js
@@ -2,76 +2,112 @@ import { shallowMount } from '@vue/test-utils'
 import FormInput from '../../src/components/FormInput.vue';
 
 describe('FormInput', () => {
-  let wrapper;
-  beforeEach(() => {
-    wrapper = shallowMount(FormInput); 
-  })
+  const factory = (propsData) => {
+    return shallowMount(FormInput, {
+      propsData: {
+        ...propsData
+      }
+    });
+  }
 
   it('renders the component', () => {
+    const wrapper = factory();
     expect(wrapper.html()).toContain('input');
+  });
+
+  it('should have an empty value on mount', () => {
+    const wrapper = factory();
+    expect(wrapper.find('input').isEmpty()).toBe(true);
+  });
+
+  it('should emit a value on mount', () => {
+    const wrapper = factory();
+    expect(wrapper.emitted().input).toBeTruthy();
+  });
+
+  it('should emit the value when input changes', () => {
+    const wrapper = factory();
+    const value = 'Hello World';
+    
+    wrapper.find('input').setValue(value);
+    expect(wrapper.emitted().input[1]).toEqual([value]);
+  });
+
+  it('sets the value on input', () => {
+    const wrapper = factory();
+    const value = 'Hello World';
+
+    wrapper.setProps({value: value});
+    expect(wrapper.find('input').element.value).toBe(value);
+  });
+
+  it('should update the value when a initial value is set and the input changes', () => {
+    const value = 'Hello World';
+    const newVal = 'Goodbye World';
+    const wrapper = factory({
+      value: value
+    });
+    expect(wrapper.find('input').element.value).toBe(value);
+    
+    wrapper.setProps({
+      value: newVal
+    });
+    expect(wrapper.find('input').element.value).toBe(newVal);
   });
 
   it('renders all configured properties', () => {
     const labelText = 'Form Input Label';
     const helperText = 'This is some text';
     const nameText = 'FormInputField';
-    const errorText = 'This field has an error';
     const placeholderText = "This is a placeholder";
-    const vModel = 'bar';
-
-    wrapper.setProps({
+    const errorText = 'This field has an error';
+    const requiredText = 'The FormInputField field is required.';
+  
+    const wrapper = factory({
       label: labelText,
       helper: helperText,
       name: nameText,
-      error: errorText,
-      validation: 'required',
       placeholder: placeholderText,
-      value: vModel
+      error: errorText,
+      validation: 'required'
     });
-    
+
     expect(wrapper.html()).toContain(labelText);
     expect(wrapper.html()).toContain(helperText);
     expect(wrapper.find('input').element.name).toBe(nameText);
     expect(wrapper.find('input').element.placeholder).toBe(placeholderText);
-    expect(wrapper.find('input').element.value).toBe(vModel);
-    
+    expect(wrapper.find('input').classes('is-invalid')).toBe(true);
     expect(wrapper.find('.invalid-feedback').exists()).toBe(true);
     expect(wrapper.find('.invalid-feedback').isVisible()).toBe(true);
-    expect(wrapper.find('.invalid-feedback').text()).toContain(errorText);
-    expect(wrapper.find('.invalid-feedback').text()).toContain('The FormInputField is required.');
-    expect(wrapper.find('input').classes('is-invalid')).toBe(true);
+    expect(wrapper.html()).toContain(errorText);
+    expect(wrapper.html()).toContain(requiredText);
   });
 
-  it('updates and emits values', () => {
-    // Should emit on mount
-    expect(wrapper.emitted().input).toBeTruthy();
-  
-    wrapper.setProps({value: 'Bar'});
-    expect(wrapper.vm.value).toBe('Bar');
-    expect(wrapper.emitted().input.length).toBe(2);
-    expect(wrapper.emitted().input[1]).toEqual(['Bar']);
-  });
-
-  it('runs validation', () => {
+  it('displays validation error messages when the field is invalid', () => {
     const requiredText = 'The FormInput field is required.';
-    const minCharText = 'The FormInput must be at least 2 characters.'
-
-    wrapper.setProps({
-      name: 'FormInput',
-      validation: 'required|min:2',
+    const errorText = 'This field has an error';
+    const wrapper = factory({
+      name: 'formInputName',
+      error: errorText,
+      validation: 'required'
     });
-    expect(wrapper.find('.invalid-feedback').exists()).toBe(true);
-    expect(wrapper.find('.invalid-feedback').isVisible()).toBe(true);
-    expect(wrapper.find('.invalid-feedback').text()).toBe(requiredText);
-    expect(wrapper.find('input').classes('is-invalid')).toBe(true);
-
-    wrapper.setProps({value: 'F'});
-    expect(wrapper.find('.invalid-feedback').exists()).toBe(true);
-    expect(wrapper.find('.invalid-feedback').isVisible()).toBe(true);
-    expect(wrapper.find('.invalid-feedback').text()).toBe(minCharText);
-    expect(wrapper.find('input').classes('is-invalid')).toBe(true);
     
-    wrapper.setProps({value: 'Foobar'});
+    expect(wrapper.find('input').classes('is-invalid')).toBe(true);
+    expect(wrapper.find('.invalid-feedback').isVisible()).toBe(true);
+    expect(wrapper.find('.invalid-feedback').text()).toContain(requiredText);
+    expect(wrapper.find('.invalid-feedback').text()).toContain(errorText);
+  });
+
+  it('removes the validation error messages when the field is valid.', () => {
+    const errorText = 'This field has an error';
+    const wrapper = factory({
+      name: 'formInputName',
+      error: errorText,
+      validation: 'required'
+    });
+    
+    wrapper.setProps({value: "Hello World"});
+    
     expect(wrapper.find('.invalid-feedback').exists()).toBe(false);
     expect(wrapper.find('input').classes('is-invalid')).toBe(false);
   });
