130bd6764a9520edb997be3d62ab4b8103d1d6ee
dante <dante@danteloayza.localdomain>
2019-12-17 09:54:39 -0400

    Clean code

--- src/components/FormDatePicker.vue
+++ src/components/FormDatePicker.vue
@@ -75,71 +75,56 @@
             dataFormat: {
                 immediate: true,
                 handler() {
-                    this.config.format = this.dataFormat === 'datetime' ? Object(dateUtils["d" /* getUserDateTimeFormat */])() : Object(dateUtils["c" /* getUserDateFormat */])();
+                    this.config.format = this.dataFormat === 'datetime'
+                            ? getUserDateTimeFormat()
+                            : getUserDateFormat();
 
-                    if (typeof (this.value) === 'undefined') {
-                        var newDate = moment();
-                        this.emitir(newDate);
-                    }
-                    else {
-                        this.setDate(this.value);
-                    }                }
-            },
-            value: {
-                immediate: false,
-                handler: function handler(_value) {
-                    if (typeof (_value) === 'undefined') {
-                        var newDate = moment();
-                        this.emitir(newDate);
-                    }
-                    else {
-                        this.setDate(_value);
-                    }
+                    this.emitOrSetDate(this.value);
                 }
-            }        },
-        methods: {
-            emitir: function (fecha) {
-                var porEmitir = this.emitIso ? fecha.toISOString() : fecha.format(this.config.format);
-                console.log('>>Emitiendo: ', porEmitir);
-                this.$emit('input', porEmitir);
             },
-            fechita: function(fecha) {
-                console.log('fechita', fecha);
-                moment_timezone_default().tz.setDefault(this.config.timeZone);
-                if (fecha) {
-                    return moment_timezone_default()(fecha, this.config.format).tz(this.config.timeZone);
+            value(_value) {
+              this.emitOrSetDate(_value);
+            }
+        },
+        methods: {
+            emitOrSetDate(date) {
+                moment.tz.setDefault(this.config.timeZone);
+                if (typeof (date) === 'undefined') {
+                    this.emitDate(moment());
                 }
                 else {
-                    return moment_timezone_default()().tz(this.config.timeZone);
+                    this.setDate(date);
+                }
+            },
+            emitDate: function (date) {
+                var toEmit = this.emitIso ? date.toISOString() : date.format(this.config.format);
+                this.$emit('input', toEmit);
+            },
+            dateInUserTimeZone: function(dateString) {
+                moment.tz.setDefault(this.config.timeZone);
+                if (dateString) {
+                    return moment(dateString, this.config.format).tz(this.config.timeZone);
+                }
+                else {
+                    return moment.tz(this.config.timeZone);
                 }
             },
             setDate(date) {
-                console.log('----------');
-                console.log('** setDate: ', date);
                 if (typeof (date) === 'undefined') {
-                    var newDate = moment();
-                    this.emitir(newDate);
+                    this.emitDate(moment());
                     return;
                 }
-                moment_timezone_default().tz.setDefault(this.config.timeZone);
-                var currentDate = moment_timezone_default()(this.date, this.config.format).tz(this.config.timeZone);
-                var newDate = moment_timezone_default.a.tz(moment_timezone_default()(date, this.config.format).format('YYYY-MM-DDTHH:mm:ss'), 'YYYY-MM-DDTHH:mm:ss', this.config.timeZone);
-
-                console.log('currentDate: ' + currentDate.toString());
-                console.log('newDate: ' + newDate.toString());
-                //console.log('moment(date):' + moment_timezone_default()(date).toString());
-                //console.log('moment(date, ZH)' + moment_timezone_default()(date, this.config.timeZone).toString());
-                //console.log('moment(date, LP)' + moment_timezone_default()(date, 'America/La_Paz').toString());
+                moment.tz.setDefault(this.config.timeZone);
+                var currentDate = moment(this.date, this.config.format).tz(this.config.timeZone);
+                var newDate = moment.tz(moment(date, this.config.format).format('YYYY-MM-DDTHH:mm:ss'), 'YYYY-MM-DDTHH:mm:ss', this.config.timeZone);
 
                 if (newDate.isSame(currentDate, 'minute') || !newDate.isValid()) {
-                    this.emitir(currentDate);
+                    this.emitDate(currentDate);
                     return;
                 }
 
                 this.date = newDate;
-                console.log('Emitiendo: '+ newDate.format(this.config.format));
-                console.log('----------');
-                this.emitir(newDate);
+                this.emitDate(newDate);
             }
         },
     };
