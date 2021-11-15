2a679210fe5011a220ed99b020bb3daeedfa82dc
Jose Chirivella <jose.chirivella@processmaker.com>
2021-11-15 14:19:11 -0500

    Added default exports to clean the index.js

--- src/components/index.js
+++ src/components/index.js
@@ -1,18 +1,18 @@
-// Import our components
-import FormInput from './FormInput'
-import FormCheckbox from './FormCheckbox'
-import FormRadioButtonGroup from './FormRadioButtonGroup'
-import FormSelect from './FormSelect'
-import FormSelectList from './FormSelectList'
-import FormTextArea from './FormTextArea'
-import FormDatePicker from './FormDatePicker'
-import FormAccordion from './FormAccordion'
-import FormHtmlEditor from './FormHtmlEditor'
-import FormHtmlViewer from './FormHtmlViewer'
-import FormDelayTimeControl from './FormDelayTimeControl'
-import FormMultiSelect from './FormMultiSelect';
-import FormPlainMultiSelect from './FormPlainMultiSelect';
-import * as dateUtils from '../dateUtils';
+// Export our components
+export {default as FormInput} from './FormInput'
+export {default as FormCheckbox} from './FormCheckbox'
+export {default as FormRadioButtonGroup} from './FormRadioButtonGroup'
+export {default as FormSelect} from './FormSelect'
+export {default as FormSelectList} from './FormSelectList'
+export {default as FormTextArea} from './FormTextArea'
+export {default as FormDatePicker} from './FormDatePicker'
+export {default as FormAccordion} from './FormAccordion'
+export {default as FormHtmlEditor} from './FormHtmlEditor'
+export {default as FormHtmlViewer} from './FormHtmlViewer'
+export {default as FormDelayTimeControl} from './FormDelayTimeControl'
+export {default as FormMultiSelect} from './FormMultiSelect';
+export {default as FormPlainMultiSelect} from './FormPlainMultiSelect';
+export * as dateUtils from '../dateUtils';
 
 // Export our components
 let components = {
@@ -29,24 +29,6 @@ let components = {
     FormDelayTimeControl,
     FormMultiSelect,
     FormPlainMultiSelect,
-}
-
-// Export our named exports
-export {
-    FormInput,
-    FormCheckbox,
-    FormRadioButtonGroup,
-    FormSelect,
-    FormSelectList,
-    FormTextArea,
-    FormDatePicker,
-    FormAccordion,
-    FormHtmlEditor,
-    FormHtmlViewer,
-    FormDelayTimeControl,
-    FormMultiSelect,
-    FormPlainMultiSelect,
-    dateUtils,
 }
 
 // Export our Vue plugin as our default
