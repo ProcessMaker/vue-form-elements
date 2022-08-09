fb4be816b99d22b9ff7bef41035c0aa5cfb12928
Teisha McRae <mcraeteisha@gmail.com>
2022-08-09 14:28:34 -0400

    Update eslint configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,37 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true,
+    es2020: true,
+    node: true,
   },
+
   extends: [
     'plugin:vue/recommended',
     'airbnb-base',
-    'plugin:prettier/recommended'
+    'plugin:prettier/recommended',
   ],
+
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
-  plugins: [
-    'vue',
-    'prettier'
-  ],
+
+  plugins: ['vue', 'prettier'],
+  
   rules: {
-    'prettier/prettier': 'error'
+    'prettier/prettier': 'error',
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
   },
+
+  root: true,
+
+  overrides: [
+    {
+      files: ['**/__tests__/*.{j,t}s?(x)', '**/tests/unit/**/*.spec.{j,t}s?(x)'],
+      env: {
+        jest: true,
+      },
+    },
+  ],
 };
