b33fb637a6f5030ab65b90417cfbd7f369ad9b8c
Rodrigo Quelca <rodrigo@processmaker.com>
2022-09-20 22:01:44 -0400

    FOUR-6760: Fix the multiple refresh with RichText Control

--- src/components/FormHtmlEditor.vue
+++ src/components/FormHtmlEditor.vue
@@ -1,111 +1,59 @@
 <template>
-  <div class='form-group'>
-    <div :class="classList">
+  <div class="form-group">
+    <div>
       <editor
         v-if="!$attrs.disabled"
         v-on="$listeners"
-        v-bind="$attrs"
-        :value="rendered"
+        :value="content"
         :init="editorSettings"
       />
       <div v-else v-html="rendered"></div>
     </div>
-    <div v-if="(validator && validator.errorCount) || error" class="invalid-feedback">
-      <div v-for="(error, index) in validator.errors.get(this.name)" :key="index">{{error}}</div>
-      <div v-if="error">{{error}}</div>
-    </div>
-    <small v-if='helper' class='form-text text-muted'>{{helper}}</small>
   </div>
 </template>
 
-
 <script>
-import { createUniqIdsMixin } from 'vue-uniq-ids'
-import ValidationMixin from './mixins/validation'
-import Mustache from 'mustache';
-import Editor from './Editor'
-import { formatIfDate } from '../dateUtils'
+import { createUniqIdsMixin } from "vue-uniq-ids";
+import ValidationMixin from "./mixins/validation";
+import Editor from "./Editor";
 
 // Create the mixin
-const uniqIdsMixin = createUniqIdsMixin()
+const uniqIdsMixin = createUniqIdsMixin();
 
 export default {
+  name: "FormHtmlEditor",
   inheritAttrs: false,
   mixins: [uniqIdsMixin, ValidationMixin],
   components: {
     Editor
   },
   props: [
-    'error',
-    'name',
-    'helper',
-    'controlClass',
-    'content',
-    'validationData',
-    'label',
-    'renderVarHtml',
-    // 'value'
+    "error",
+    "name",
+    "helper",
+    "controlClass",
+    "content",
+    "validationData",
+    "label",
+    "renderVarHtml"
   ],
-  computed:{
-    classList(){
-      let classList = {
-        'is-invalid': (this.validator && this.validator.errorCount) || this.error,
-      }
-      if(this.controlClass){
-        classList[this.controlClass] = true
-      }
-      return classList
-    },
-    rendered() {
-      if (!this.validationData) {
-        return this.content;
-      }
-
-      this.originalEscapeFn = Mustache.escape;
-      Mustache.escape = this.mustacheEscapeFn;
-      try {
-        const parent = Object.assign({_parent: this.validationData._parent}, this.validationData);
-        if (this.renderVarHtml) {
-          return Mustache.render(this.content, {...this.customFunctions, ...this.validationData, ...parent});
-        }
-        return Mustache.render(this.content, {...this.customFunctions, ...this.validationData, ...parent});
-      } catch (error) {
-        if (this.renderVarHtml) {
-          return this.renderVarName;
-        }
-        return this.content;
-      } finally {
-        Mustache.escape = this.originalEscapeFn;
-      }
-    }
-  },
-  methods: {
-    registerCustomFunction(name, implementation) {
-      this.customFunctions[name] = implementation;
-    },
-    mustacheEscapeFn(text) {
-      text = formatIfDate(text);
-      if (this.renderVarHtml) {
-        return text;
-      }
-      return this.originalEscapeFn(text);
-    }
-  },
   data() {
     return {
       customFunctions: {},
       editorSettings: {
         inline: true,
         menubar: false,
-        plugins: [ 'link', 'lists' ],
-        toolbar: 'undo redo | link | styleselect | bold italic forecolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent',
+        plugins: ["link", "lists"],
+        toolbar: `undo redo | link | styleselect | bold italic forecolor |
+          alignleft aligncenter alignright alignjustify |
+          bullist numlist outdent indent`,
         skin: false,
         relative_urls: false,
-        remove_script_host: false,
-      },
-    }
+        remove_script_host: false
+      }
+    };
   }
-}
+};
 </script>
 
 <style scoped>
