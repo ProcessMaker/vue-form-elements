e532c07bab36d595eb6d6997904ba8082987e0a2
Rodrigo Quelca <rodrigo.quelca@gmail.com>
2022-09-21 11:46:15 -0400

    FOUR-6760: Fix the multiple refresh with RichText Control
    add some code comments
    
    remove console.log
    
    fix some comments

--- src/components/FormHtmlEditor.vue
+++ src/components/FormHtmlEditor.vue
@@ -1,74 +1,92 @@
 <template>
-  <div class='form-group'>
+  <div class="form-group">
     <div :class="classList">
       <editor
         v-if="!$attrs.disabled"
-        v-on="$listeners"
-        v-bind="$attrs"
         :value="rendered"
         :init="editorSettings"
+        v-bind="$attrs"
+        v-on="$listeners"
       />
-      <div v-else v-html="rendered"></div>
+      <div v-else>{{ rendered }}</div>
     </div>
-    <div v-if="(validator && validator.errorCount) || error" class="invalid-feedback">
-      <div v-for="(error, index) in validator.errors.get(this.name)" :key="index">{{error}}</div>
-      <div v-if="error">{{error}}</div>
+    <div
+      v-if="(validator && validator.errorCount) || error"
+      class="invalid-feedback"
+    >
+      <div
+        v-for="(error, index) in validator.errors.get(this.name)"
+        :key="index"
+      >
+        {{ error }}
+      </div>
+      <div v-if="error">{{ error }}</div>
     </div>
-    <small v-if='helper' class='form-text text-muted'>{{helper}}</small>
+    <small v-if="helper" class="form-text text-muted">{{ helper }}</small>
   </div>
 </template>
 
-
 <script>
-import { createUniqIdsMixin } from 'vue-uniq-ids'
-import ValidationMixin from './mixins/validation'
-import Mustache from 'mustache';
-import Editor from './Editor'
-import { formatIfDate } from '../dateUtils'
+import { createUniqIdsMixin } from "vue-uniq-ids";
+import Mustache from "mustache";
+import ValidationMixin from "./mixins/validation";
+import Editor from "./Editor";
+import { formatIfDate } from "../dateUtils";
 
 // Create the mixin
-const uniqIdsMixin = createUniqIdsMixin()
+const uniqIdsMixin = createUniqIdsMixin();
 
 export default {
-  inheritAttrs: false,
-  mixins: [uniqIdsMixin, ValidationMixin],
   components: {
     Editor
   },
+  mixins: [uniqIdsMixin, ValidationMixin],
+  inheritAttrs: false,
   props: [
-    'error',
-    'name',
-    'helper',
-    'controlClass',
-    'content',
-    'validationData',
-    'label',
-    'renderVarHtml',
-    // 'value'
+    "error",
+    "name",
+    "helper",
+    "controlClass",
+    "content",
+    "validationData",
+    "label",
+    "renderVarHtml"
   ],
-  computed:{
-    classList(){
-      let classList = {
-        'is-invalid': (this.validator && this.validator.errorCount) || this.error,
+  data() {
+    return {
+      originalEscapeFn: null,
+      customFunctions: {},
+      editorSettings: {
+        inline: true,
+        menubar: false,
+        plugins: ["link", "lists"],
+        toolbar: `undo redo | link | styleselect | bold italic forecolor |
+           alignleft aligncenter alignright alignjustify | bullist numlist outdent indent`,
+        skin: false,
+        relative_urls: false,
+        remove_script_host: false
       }
-      if(this.controlClass){
-        classList[this.controlClass] = true
+    };
+  },
+  computed: {
+    classList() {
+      const classList = {
+        "is-invalid":
+          (this.validator && this.validator.errorCount) || this.error
+      };
+      if (this.controlClass) {
+        classList[this.controlClass] = true;
       }
-      return classList
+      return classList;
     },
     rendered() {
-      if (!this.validationData) {
-        return this.content;
-      }
-
-      this.originalEscapeFn = Mustache.escape;
-      Mustache.escape = this.mustacheEscapeFn;
+      const data = this.makeProxyData();
+      this.overwriteMustacheEscape();
       try {
-        const parent = Object.assign({_parent: this.validationData._parent}, this.validationData);
         if (this.renderVarHtml) {
-          return Mustache.render(this.content, {...this.customFunctions, ...this.validationData, ...parent});
+          return Mustache.render(this.content, data);
         }
-        return Mustache.render(this.content, {...this.customFunctions, ...this.validationData, ...parent});
+        return Mustache.render(this.content, data);
       } catch (error) {
         if (this.renderVarHtml) {
           return this.renderVarName;
@@ -80,32 +98,65 @@ export default {
     }
   },
   methods: {
+    /**
+     * Create a proxy for an empty object. in order to avoid unespected refresh
+     * @return {object} proxy
+     */
+    makeProxyData() {
+      const control = this;
+      const handler = {
+        get: (target, name) => {
+          if (control.customFunctions[name]) {
+            return control.customFunctions[name];
+          }
+          if (name === "_parent") {
+            return (
+              control.validationData._parent || control.validationData._parent
+            );
+          }
+          return control.validationData[name];
+        },
+        has(target, name) {
+          if (control.customFunctions[name]) {
+            return true;
+          }
+          if (name === "_parent") {
+            return true;
+          }
+          return control.validationData[name] !== undefined;
+        }
+      };
+      return new Proxy({}, handler);
+    },
+    /**
+     * Backup and overwrite the original mustache escape property
+     */
+    overwriteMustacheEscape() {
+      this.originalEscapeFn = Mustache.escape;
+      Mustache.escape = this.mustacheEscapeFn;
+    },
+    /**
+     * Register custom functions to be included
+     * @param {string} name
+     * @param {object} implementation
+     */
     registerCustomFunction(name, implementation) {
       this.customFunctions[name] = implementation;
     },
+    /**
+     * Escape the mustache code, added in the tinyMCE editor
+     * @param {string} text
+     * @return {object}
+     */
     mustacheEscapeFn(text) {
-      text = formatIfDate(text);
+      const formatedText = formatIfDate(text);
       if (this.renderVarHtml) {
-        return text;
+        return formatedText;
       }
-      return this.originalEscapeFn(text);
-    }
-  },
-  data() {
-    return {
-      customFunctions: {},
-      editorSettings: {
-        inline: true,
-        menubar: false,
-        plugins: [ 'link', 'lists' ],
-        toolbar: 'undo redo | link | styleselect | bold italic forecolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent',
-        skin: false,
-        relative_urls: false,
-        remove_script_host: false,
-      },
+      return this.originalEscapeFn(formatedText);
     }
   }
-}
+};
 </script>
 
 <style scoped>
