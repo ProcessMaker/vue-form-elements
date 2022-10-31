a41627418230fea6c6fde283667d9e56ae867094
Jose Chirivella <jose.chirivella@processmaker.com>
2022-10-31 17:06:34 -0500

    FOUR-6854 Replaced the Datepicker component and ran the prettier/linter in this file

--- src/components/FormDatePicker.vue
+++ src/components/FormDatePicker.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="form-group position-relative">
-    <label v-uni-for="name">{{label}}</label>
-    <date-picker
+    <label v-uni-for="name">{{ label }}</label>
+    <date-pick
       v-model="date"
       :config="config"
       :disabled="disabled"
@@ -12,57 +12,66 @@
       :class="classList"
     />
     <div v-if="errors.length > 0" class="invalid-feedback d-block">
-      <div v-for="(error, index) in errors" :key="index">{{error}}</div>
+      <div v-for="(error, index) in errors" :key="index">{{ error }}</div>
     </div>
-    <small v-if="helper" class="form-text text-muted">{{helper}}</small>
+    <small v-if="helper" class="form-text text-muted">{{ helper }}</small>
   </div>
 </template>
 
 <script>
-import { createUniqIdsMixin } from 'vue-uniq-ids';
-import ValidationMixin from './mixins/validation';
+import { createUniqIdsMixin } from "vue-uniq-ids";
+import DatePick from "vue-date-pick";
+import moment from "moment-timezone";
+import Mustache from "mustache";
+import ValidationMixin from "./mixins/validation";
 import DataFormatMixin from "./mixins/DataFormat";
-import datePicker from 'vue-bootstrap-datetimepicker';
-import moment from 'moment-timezone';
-import { getLang, getUserDateFormat, getUserDateTimeFormat } from '../dateUtils';
-import Mustache from 'mustache';
-let Validator = require('validatorjs');
+import {
+  getLang,
+  getUserDateFormat,
+  getUserDateTimeFormat
+} from "../dateUtils";
+import "vue-date-pick/dist/vueDatePick.css";
+
+const Validator = require("validatorjs");
 
 const uniqIdsMixin = createUniqIdsMixin();
-const checkFormats = ['YYYY-MM-DD', moment.ISO_8601];
+const checkFormats = ["YYYY-MM-DD", moment.ISO_8601];
 
-Validator.register('date_or_mustache', function(value, requirement, attribute) {
-  let rendered = null;
-  try {
-    // Clear out any mustache statements
-    rendered = Mustache.render(value, {});
-  } catch (e) {
-    rendered = value;
-  }
+Validator.register(
+  "date_or_mustache",
+  function (value, requirement, attribute) {
+    let rendered = null;
+    try {
+      // Clear out any mustache statements
+      rendered = Mustache.render(value, {});
+    } catch (e) {
+      rendered = value;
+    }
 
-  if (value !== rendered) {
-    // contains mustache, so just give them the benefit of the doubt here
-    return true;
-  }
+    if (value !== rendered) {
+      // contains mustache, so just give them the benefit of the doubt here
+      return true;
+    }
 
-  if (value === '') {
-    // Empty is ok, it just disables min/max
-    return true;
-  }
+    if (value === "") {
+      // Empty is ok, it just disables min/max
+      return true;
+    }
 
-  if (moment(value, checkFormats, true).isValid()) {
-    return true;
-  }
+    if (moment(value, checkFormats, true).isValid()) {
+      return true;
+    }
 
-  return false;
-
-}, 'Must be YYYY-MM-DD, ISO8601, or mustache syntax');
+    return false;
+  },
+  "Must be YYYY-MM-DD, ISO8601, or mustache syntax"
+);
 
 export default {
-  mixins: [uniqIdsMixin, ValidationMixin, DataFormatMixin],
   components: {
-    datePicker
+    DatePick
   },
+  mixins: [uniqIdsMixin, ValidationMixin, DataFormatMixin],
   props: {
     name: String,
     placeholder: String,
@@ -71,31 +80,24 @@ export default {
     helper: String,
     dataFormat: String,
     value: [String, Boolean, Date],
-    inputClass: {type: [String, Array, Object], default: 'form-control'},
+    inputClass: { type: [String, Array, Object], default: "form-control" },
     dataTest: String,
     disabled: null,
     minDate: { type: [String, Boolean], default: false },
-    maxDate: { type: [String, Boolean], default: false },
+    maxDate: { type: [String, Boolean], default: false }
   },
   data() {
     return {
       validatorErrors: [],
-      date: null,
-    }
-  },
-  created() {
-    Validator.register('after_min_date', (value, requirement, attribute) => {
-      if (this.parseDate(value) < this.parseDate(this.minDate)) {
-        return false;
-      }
-      return true;
-    }, 'Must be after or equal Minimum Date');
+      date: ""
+    };
   },
   computed: {
     classList() {
       return {
-        'is-invalid': (this.validator && this.validator.errorCount) || this.error,
-      }
+        "is-invalid":
+          (this.validator && this.validator.errorCount) || this.error
+      };
     },
     errors() {
       if (this.error) {
@@ -105,52 +107,58 @@ export default {
     },
     config() {
       return {
-        format: this.dataFormat === 'datetime' ? getUserDateTimeFormat() : getUserDateFormat(),
+        format:
+          this.dataFormat === "datetime"
+            ? getUserDateTimeFormat()
+            : getUserDateFormat(),
         locale: getLang(),
         useCurrent: false,
         showClose: true,
         minDate: this.parseDate(this.minDate),
         maxDate: this.checkValidMaxDate(),
         icons: {
-          time: 'far fa-clock',
-          date: 'far fa-calendar',
-          up: 'fas fa-arrow-up',
-          down: 'fas fa-arrow-down',
-          previous: 'fas fa-chevron-left',
-          next: 'fas fa-chevron-right',
-          today: 'fas fa-calendar-check',
-          clear: 'far fa-trash-alt',
-          close: 'far fa-times-circle'
+          time: "far fa-clock",
+          date: "far fa-calendar",
+          up: "fas fa-arrow-up",
+          down: "fas fa-arrow-down",
+          previous: "fas fa-chevron-left",
+          next: "fas fa-chevron-right",
+          today: "fas fa-calendar-check",
+          clear: "far fa-trash-alt",
+          close: "far fa-times-circle"
         }
       };
-    },
+    }
   },
   watch: {
     validator: {
       deep: true,
       handler() {
-        this.validatorErrors = this.validator && this.validator.errors.get(this.name)
-          ? this.validator.errors.get(this.name)
-          : [];
-
-      },
+        this.validatorErrors =
+          this.validator && this.validator.errors.get(this.name)
+            ? this.validator.errors.get(this.name)
+            : [];
+      }
     },
     date() {
       if (this.value && !this.date) {
-        this.$emit('input', '');
+        this.$emit("input", "");
       }
 
       if (this.isDateAndValueTheSame()) {
         return;
       }
 
-      const newDate = this.dataFormat === 'date' ? moment.utc(this.date, this.config.format) : moment(this.date, this.config.format);
+      const newDate =
+        this.dataFormat === "date"
+          ? moment.utc(this.date, this.config.format)
+          : moment(this.date, this.config.format);
 
-      this.$emit('input', newDate.toISOString());
+      this.$emit("input", newDate.toISOString());
     },
     value() {
       if (!this.value) {
-        this.date = '';
+        this.date = "";
         return;
       }
 
@@ -165,13 +173,22 @@ export default {
       handler() {
         this.date = this.value
           ? this.generateDate().format(this.config.format)
-          : '';
+          : "";
       }
-    },
+    }
+  },
+  created() {
+    Validator.register(
+      "after_min_date",
+      (value, requirement, attribute) => {
+        return this.parseDate(value) >= this.parseDate(this.minDate);
+      },
+      "Must be after or equal Minimum Date"
+    );
   },
   methods: {
     checkValidMaxDate() {
-      if (this.minDate == '') {
+      if (this.minDate === "") {
         return this.parseDate(this.maxDate);
       }
       if (this.parseDate(this.maxDate) >= this.parseDate(this.minDate)) {
@@ -182,8 +199,7 @@ export default {
     parseDate(val) {
       let date = false;
 
-      if (typeof val === 'string' && val !== '') {
-
+      if (typeof val === "string" && val !== "") {
         try {
           date = Mustache.render(val, this.validationData);
         } catch (error) {
@@ -205,7 +221,7 @@ export default {
 
       const currentDate = moment(this.date, this.config.format);
       const currentValue = this.value ? moment(this.value) : null;
-      const comparatorString = this.dataFormat !== 'datetime' ? 'day' : null;
+      const comparatorString = this.dataFormat !== "datetime" ? "day" : null;
 
       return currentDate.isSame(currentValue, comparatorString);
     },
@@ -217,15 +233,7 @@ export default {
       }
 
       return date;
-    },
-  },
+    }
+  }
 };
 </script>
-
-<style>
-  @import '~pc-bootstrap4-datetimepicker/build/css/bootstrap-datetimepicker.css';
-
-  .inspector-container .bootstrap-datetimepicker-widget.dropdown-menu {
-    font-size: 11px;
-  }
-</style>
