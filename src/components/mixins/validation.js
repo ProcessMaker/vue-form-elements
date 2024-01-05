8855abf8da362fd84bbbf70f049a6ef94f122093
Jose Chirivella <jose.chirivella@processmaker.com>
2024-01-05 15:07:41 -0600

    Fixed the validatorjs issue by locking the version to when it worked

--- src/components/mixins/validation.js
+++ src/components/mixins/validation.js
@@ -1,21 +1,19 @@
 import Validator from "@chantouchsek/validatorjs";
-import moment from "moment";
-import { has, get } from "lodash";
+import moment from "moment-timezone";
+import ProxyData from "./ProxyData";
+import { get, has } from "lodash";
 
 export default {
   name: "ValidationMixin",
-  props: [
-    "validation",
-    "validationData",
-    "validationField",
-    "validationMessages"
-  ],
+  mixins: [ProxyData],
+  props: ["validation", "validationData", "validationField", "validationMessages", "config"],
   computed: {
     isReadOnly() {
-      return !!(this.readonly ||
-        this.disabled ||
-        this.$attrs.readonly ||
-        this.$attrs.disabled);
+      if (this.readonly || this.disabled || this.$attrs.readonly || this.$attrs.disabled) {
+        return true;
+      } else {
+        return false;
+      }
     },
     required() {
       for (const validation of get(this.config, "validation", []) || []) {
@@ -45,20 +43,19 @@ export default {
             }
           }
         }
-
       }
       return false;
     }
+  },
+  data() {
+    return {
+      validator: null
+    };
   },
   mounted() {
     this.setValidatorLanguage();
     this.updateValidation();
     this.observeElementMutations();
-  },
-  data() {
-    return {
-      validator: {}
-    };
   },
   watch: {
     // Triggered whenever the v-model is updated
@@ -79,77 +76,13 @@ export default {
       this.updateValidation();
     },
     validationData: {
-      handler() {
+      handler: function () {
         this.updateValidation();
       },
       deep: true
     }
   },
   methods: {
-    /**
-     * Create a proxy for an empty object. in order to avoid unespected refresh
-     * @return {object} proxy
-     */
-    makeProxyData() {
-      const control = this;
-      const handler = {
-        get: (target, name) => {
-          // customFunctions is used by RichText controls
-          // to add custom Mustache functions
-          if (control.customFunctions && control.customFunctions[name]) {
-            return control.customFunctions[name];
-          }
-          if (name === "_parent") {
-            const screenOwner = control.getScreenOwner();
-            return (
-              (screenOwner && screenOwner._parent) || // Get _parent for the current screen (e.g. Inside Loops, Inside Tabs?, RecordLists...?)
-              control.validationData._parent
-            ); // Get _parent for the Request Data (e.g. Inside a SubProcess)
-          }
-          // Check if validationData is empty
-          if (
-            control.validationData === undefined ||
-            control.validationData === null
-          ) {
-            return undefined;
-          }
-          return control.validationData[name];
-        },
-        has(target, name) {
-          // customFunctions is used by RichText controls
-          // to add custom Mustache functions
-          if (control.customFunctions && control.customFunctions[name]) {
-            return true;
-          }
-          if (name === "_parent") {
-            return true;
-          }
-          // Check if validationData is empty
-          if (
-            control.validationData === undefined ||
-            control.validationData === null
-          ) {
-            return false;
-          }
-          return control.validationData[name] !== undefined;
-        }
-      };
-      return new Proxy({}, handler);
-    },
-    /**
-     * Gets the screen parent or null if don't have
-     * @returns {object|null}
-     */
-    getScreenOwner() {
-      let parent = this.$parent;
-      while (parent) {
-        if (parent.$options.name === "ScreenContent") {
-          return parent;
-        }
-        parent = parent.$parent;
-      }
-      return null;
-    },
     observeElementMutations() {
       new MutationObserver(this.handleMutations).observe(this.$el, {
         attributes: true,
@@ -182,9 +115,7 @@ export default {
     updateValidation() {
       if (this.validation && !this.isReadOnly) {
         let fieldName = this.validationField ? this.validationField : this.name;
-        let data = this.validationData
-          ? this.validationData
-          : { [fieldName]: this.value };
+        let data = this.validationData ? this.validationData : { [fieldName]: this.value };
         let validationRules = "";
 
         if (typeof this.validation !== "string" && this.validation.length) {
@@ -207,9 +138,7 @@ export default {
         };
         this.registerCustomRules(data);
         this.validator = new Validator(data, rules, {
-          validationMessages: this.validationMessages
-            ? this.validationMessages
-            : null
+          validationMessages: this.validationMessages ? this.validationMessages : null
         });
         this.validator.setAttributeNames({ [fieldName]: this.label });
         this.validator.errors.first(this.name);
@@ -222,166 +151,158 @@ export default {
     registerCustomRules(data) {
       Validator.register(
         "custom-same",
-          function(val, req) {
-            let val1;
-            let val2 = val;
-            if (!req.includes(".")) {
-              val1 = this.validator._flattenObject(this.validator.input)[req];
-            } else {
-              val1 = req
-                .split(".")
-                .reduce((obj, i) => obj[i], this.validator.input);
-            }
+        function (val, req) {
+          let val1;
+          let val2 = val;
+          if (!req.includes(".")) {
+            val1 = this.validator._flattenObject(this.validator.input)[req];
+          } else {
+            val1 = req.split(".").reduce((obj, i) => obj[i], this.validator.input);
+          }
 
-            if (val1 === val2) {
-              return true;
-            }
+          if (val1 === val2) {
+            return true;
+          }
 
-            return false;
-          },
-          "The :attribute and :custom-same fields must match."
+          return false;
+        },
+        "The :attribute and :custom-same fields must match."
       );
 
       Validator.register(
         "after",
-          function(date, params) {
-            // checks if incoming 'params' is a date or a key reference.
-            let checkDate = moment(params);
-            if (!checkDate.isValid()) {
-              params = data[params];
-            }
+        function (date, params) {
+          // checks if incoming 'params' is a date or a key reference.
+          let checkDate = moment(params);
+          if (!checkDate.isValid()) {
+            params = data[params];
+          }
 
-            const inputDate = moment(date).toISOString();
-            const afterDate = moment(params).toISOString();
+          const inputDate = moment(date).toISOString();
+          const afterDate = moment(params).toISOString();
 
-            return inputDate > afterDate;
-          },
-          "The :attribute must be after :after."
+          return inputDate > afterDate;
+        },
+        "The :attribute must be after :after."
       );
 
       Validator.register(
         "after_or_equal",
-          function(date, params) {
-            // checks if incoming 'params' is a date or a key reference.
-            let checkDate = moment(params);
-            if (!checkDate.isValid()) {
-              params = data[params];
-            }
+        function (date, params) {
+          // checks if incoming 'params' is a date or a key reference.
+          let checkDate = moment(params);
+          if (!checkDate.isValid()) {
+            params = data[params];
+          }
 
-            const inputDate = moment(date).toISOString();
-            const equalOrAfterDate = moment(params).toISOString();
+          const inputDate = moment(date).toISOString();
+          const equalOrAfterDate = moment(params).toISOString();
 
-            return inputDate >= equalOrAfterDate;
-          },
-          "The :attribute must be equal or after :after_or_equal."
+          return inputDate >= equalOrAfterDate;
+        },
+        "The :attribute must be equal or after :after_or_equal."
       );
 
       Validator.register(
         "before",
-          function(date, params) {
-            // checks if incoming 'params' is a date or a key reference.
-            let checkDate = moment(params);
-            if (!checkDate.isValid()) {
-              params = data[params];
-            }
+        function (date, params) {
+          // checks if incoming 'params' is a date or a key reference.
+          let checkDate = moment(params);
+          if (!checkDate.isValid()) {
+            params = data[params];
+          }
 
-            const inputDate = moment(date).toISOString();
-            const beforeDate = moment(params).toISOString();
+          const inputDate = moment(date).toISOString();
+          const beforeDate = moment(params).toISOString();
 
-            return inputDate < beforeDate;
-          },
-          "The :attribute must be before :before."
+          return inputDate < beforeDate;
+        },
+        "The :attribute must be before :before."
       );
 
       Validator.register(
         "before_or_equal",
-          function(date, params) {
-            // checks if incoming 'params' is a date or a key reference.
-            let checkDate = moment(params);
-            if (!checkDate.isValid()) {
-              params = data[params];
-            }
+        function (date, params) {
+          // checks if incoming 'params' is a date or a key reference.
+          let checkDate = moment(params);
+          if (!checkDate.isValid()) {
+            params = data[params];
+          }
 
-            const inputDate = moment(date).toISOString();
-            const beforeDate = moment(params).toISOString();
+          const inputDate = moment(date).toISOString();
+          const beforeDate = moment(params).toISOString();
 
-            return inputDate <= beforeDate;
-          },
-          "The :attribute must be equal or before :before_or_equal."
+          return inputDate <= beforeDate;
+        },
+        "The :attribute must be equal or before :before_or_equal."
       );
 
       Validator.register(
         "required_if",
-          function(val, req, attribute) {
-            if (typeof req === "string") {
-              req = req.split(",");
-            }
+        function (val, req, attribute) {
+          if (typeof req === "string") {
+            req = req.split(",");
+          }
 
-            let inputtedValue = this.validator._objectPath(
-              this.validator.input,
-              req[0]
-            );
+          let inputtedValue = this.validator._objectPath(this.validator.input, req[0]);
 
-            switch (typeof inputtedValue) {
-              case "boolean":
-              case "number":
-                if (inputtedValue.toString() == req[1]) {
-                  return this.validator.getRule("required").validate(val);
-                }
-                break;
-              default:
-                if (inputtedValue == req[1]) {
-                  return this.validator.getRule("required").validate(val);
-                }
-                break;
-            }
-            return true;
-          },
-          "The :attribute field is required."
+          switch (typeof inputtedValue) {
+            case "boolean":
+            case "number":
+              if (inputtedValue.toString() == req[1]) {
+                return this.validator.getRule("required").validate(val);
+              }
+              break;
+            default:
+              if (inputtedValue == req[1]) {
+                return this.validator.getRule("required").validate(val);
+              }
+              break;
+          }
+          return true;
+        },
+        "The :attribute field is required."
       );
 
       Validator.register(
         "required_unless",
-          function(val, req, attribute) {
-            if (typeof req === "string") {
-              req = req.split(",");
-            }
+        function (val, req, attribute) {
+          if (typeof req === "string") {
+            req = req.split(",");
+          }
 
-            let inputtedValue = this.validator._objectPath(
-              this.validator.input,
-              req[0]
-            );
+          let inputtedValue = this.validator._objectPath(this.validator.input, req[0]);
 
-            switch (typeof inputtedValue) {
-              case "boolean":
-              case "number":
-                if (inputtedValue.toString() !== req[1]) {
-                  return this.validator.getRule("required").validate(val);
-                }
-                break;
-              default:
-                if (inputtedValue !== req[1]) {
-                  return this.validator.getRule("required").validate(val);
-                }
-                break;
-            }
-            return true;
-          },
-          "The :attribute field is required."
+          switch (typeof inputtedValue) {
+            case "boolean":
+            case "number":
+              if (inputtedValue.toString() !== req[1]) {
+                return this.validator.getRule("required").validate(val);
+              }
+              break;
+            default:
+              if (inputtedValue !== req[1]) {
+                return this.validator.getRule("required").validate(val);
+              }
+              break;
+          }
+          return true;
+        },
+        "The :attribute field is required."
       );
 
       Validator.register(
         "between",
-          function(value, req) {
-            const number = Number(value);
-            const min = req.split(",")[0];
-            const max = req.split(",")[1];
-            if (number >= min && number <= max) {
-              return true;
-            }
-            return false;
-          },
-          "Must have a value between :between"
+        function (value, req) {
+          const number = Number(value);
+          const min = req.split(",")[0];
+          const max = req.split(",")[1];
+          if (number >= min && number <= max) {
+            return true;
+          }
+          return false;
+        },
+        "Must have a value between :between"
       );
     }
   }
